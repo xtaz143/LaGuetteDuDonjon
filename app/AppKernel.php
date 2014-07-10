9c8f136785ec360f385c0482165f0afbd95ffa6b
xtaz143 <yoann.alonso@lyceeastier.com>
2014-07-10 21:38:14 +0200

    Add userBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new LGDD\SiteBundle\LGDDSiteBundle(),
+            new LGDD\UserBundle\LGDDUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
